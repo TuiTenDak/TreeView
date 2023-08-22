6ec5a5e7b130cc83f085dca5ca434963d6a72a90
Lưu Công Nhật Hòa <134906859+TuiTenDak@users.noreply.github.com>
2023-08-22 20:51:55 +0700

    Add files via upload

---
+++ Task05_TrueView/Task05_TrueView/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Task05_TrueView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeFolder = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeFolder
+            // 
+            this.treeFolder.AccessibleRole = System.Windows.Forms.AccessibleRole.PageTabList;
+            this.treeFolder.Location = new System.Drawing.Point(12, 32);
+            this.treeFolder.Name = "treeFolder";
+            this.treeFolder.Size = new System.Drawing.Size(411, 490);
+            this.treeFolder.TabIndex = 0;
+            this.treeFolder.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 526);
+            this.Controls.Add(this.treeFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeFolder;
+    }
+}
+
---
+++ Task05_TrueView/Task05_TrueView/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
+using static System.Net.WebRequestMethods;
+using TreeView = System.Windows.Forms.TreeView;
+
+namespace Task05_TrueView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(treeView1.SelectedNode.FullPath.ToString());
+            /*TreeNode tNode;
+            tNode = treeView1.Nodes.Add(@"Co so du lieu");
+            treeView1.Nodes[0].Nodes.Add("He co so du lieu");
+            treeView1.Nodes[0].Nodes.Add("He co so du lieu");
+            treeView1.Nodes[0].Nodes[0].Nodes.Add("CLR");*/
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Tree();
+            //treeFolder_BeforeExpand();
+            
+        }
+
+        private void Tree()
+        {
+            
+            string[] drives=Directory.GetLogicalDrives();
+            TreeNode node = null;
+            string file_Text = @"D:\1.txt";
+            //using (StreamWriter writer = new StreamWriter(file_Text))
+            //{
+                foreach (string drv in drives)
+                {
+                    node = new TreeNode(drv);  
+                    treeFolder.Nodes.Add(node);
+                    node.Nodes.Add("Temp");
+                    //writer.WriteLine(drv);
+                }
+            //}
+        }
+
+        private void treeFolder_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode node= e.Node;
+            node.Nodes.Clear();
+            node.ImageIndex = 0;
+            string file_Text = @"D:\1.txt";
+            try
+            {
+                //using (StreamWriter writer = new StreamWriter(file_Text))
+               // {
+                    foreach (string dir in Directory.GetDirectories(node.FullPath))
+                    {
+                        TreeNode n = node.Nodes.Add(Path.GetFileName(dir));
+                        //writer.WriteLine(n);
+                        n.Nodes.Add("Temp");
+                    }
+                   
+                //}
+            }
+            catch { }
+        }
+        /*private void SaveTreeViewToTextFile(TreeView treeView, string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (TreeNode node in treeView.Nodes)
+                {
+                    SaveTreeNodeToTextFile(node, writer, 0);
+                }
+            }
+        }*/
+
+       /*private void SaveTreeNodeToTextFile(TreeNode node, StreamWriter writer, int level)
+        {
+            string line = new string('-', level) + node.Text;
+            writer.WriteLine(line);
+            foreach (TreeNode childNode in node.Nodes)
+            {
+                SaveTreeNodeToTextFile(childNode, writer, level + 1);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string filePath = @"D:\1.txt";
+            SaveTreeViewToTextFile(treeFolder, filePath);
+        }
+*/
+
+    }
+}
